017514b4f7c256a5a0d1facdf05261569d5d535a
erwin97 <49305500+erwin97@users.noreply.github.com>
2019-04-08 12:33:24 +0200

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
   public static void main (String[] args) {
-    for(int i=0, i <= 100, i++) {
+    for(int i=0; i <= 100; i++) {
       if (i= 50) {
         System.out.println(Hello, World!);
       }
